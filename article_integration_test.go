9072701c3c14e06d935e82b41f51c70938ca94fe
mahdi khanzadi <khanzadimahdi@gmail.com>
2022-09-25 13:34:13 +0200

    integration test

---
+++ article_integration_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreatingAndRetrievingThem(t *testing.T) {
+	repositoy := NewInMemoryRepository()
+	server := ArticleServer{repository: repositoy}
+	article := Article{
+		Title:  "title",
+		Body:   "body",
+		Status: "draft",
+	}
+
+	body, _ := json.Marshal(article)
+
+	request1, _ := http.NewRequest(http.MethodPost, "/articles", bytes.NewReader(body))
+	request2, _ := http.NewRequest(http.MethodPost, "/articles", bytes.NewReader(body))
+	request3, _ := http.NewRequest(http.MethodPost, "/articles", bytes.NewReader(body))
+
+	server.ServeHTTP(httptest.NewRecorder(), request1)
+	server.ServeHTTP(httptest.NewRecorder(), request2)
+	server.ServeHTTP(httptest.NewRecorder(), request3)
+
+	request, _ := http.NewRequest(http.MethodGet, "/articles", nil)
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("got %d, want %d", response.Code, http.StatusOK)
+	}
+
+	articles := []Article{}
+	json.NewDecoder(response.Body).Decode(&articles)
+
+	if len(articles) != 3 {
+		t.Errorf("got %d articles, wanted %d", len(articles), 3)
+	}
+
+}
