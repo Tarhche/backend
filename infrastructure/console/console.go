e244308ce17c2dfe220cd94c2f98b058d7045785
mahdikhanzadi <khanzadimahdi@gmail.com>
2023-07-29 10:10:35 +0000

    register commands

--- infrastructure/console/console.go
+++ infrastructure/console/console.go
@@ -1,14 +1,30 @@
 package console
 
-import "context"
+import (
+	"context"
+	"reflect"
+)
+
+type Command interface {
+	Run(context.Context) int
+}
 
 type Console struct {
+	command Command
 }
 
 func NewConsole() *Console {
 	return &Console{}
 }
 
+func (c *Console) Register(command Command) {
+	c.command = command
+}
+
 func (c *Console) Run(ctx context.Context) int {
-	return 0
+	if c.command == nil || reflect.ValueOf(c.command).IsNil() {
+		return 0
+	}
+
+	return c.command.Run(ctx)
 }
--- infrastructure/console/console_test.go
+++ infrastructure/console/console_test.go
@@ -6,12 +6,51 @@ import (
 )
 
 func TestConsole(t *testing.T) {
-	console := NewConsole()
+	t.Run("run with no registered command", func(t *testing.T) {
+		console := NewConsole()
 
-	ctx := context.Background()
-	exitCode := console.Run(ctx)
+		ctx := context.Background()
+		exitCode := console.Run(ctx)
 
-	if exitCode != 0 {
-		t.Error("unexpected exit code")
-	}
+		if exitCode != 0 {
+			t.Error("unexpected exit code")
+		}
+	})
+
+	t.Run("register a command", func(t *testing.T) {
+		console := NewConsole()
+		ctx := context.Background()
+		cmd := &MockCommand{}
+
+		console.Register(cmd)
+
+		if exitCode := console.Run(ctx); exitCode != 0 {
+			t.Error("unexpected exit code")
+		}
+
+		if cmd.count != 1 {
+			t.Error("command not invoked")
+		}
+
+		cmd.count = 0
+		cmd.exitCode = 1
+		if exitCode := console.Run(ctx); exitCode != 1 {
+			t.Error("unexpected exit code")
+		}
+
+		if cmd.count != 1 {
+			t.Error("command not invoked")
+		}
+	})
+}
+
+type MockCommand struct {
+	exitCode int
+	count    int
+}
+
+func (c *MockCommand) Run(ctx context.Context) int {
+	c.count++
+
+	return c.exitCode
 }
