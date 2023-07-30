b9e600a6cc29f4e7ba7dc8bce3fe2529094a6fc3
mahdikhanzadi <khanzadimahdi@gmail.com>
2023-07-30 21:30:10 +0000

    improve console

--- infrastructure/console/console.go
+++ infrastructure/console/console.go
@@ -2,69 +2,120 @@ package console
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"io"
+	"strings"
 )
 
-// ExitStatus represents a Posix exit status that a command
-// expects to be returned to the shell.
+// ExitStatus represents a Posix exit status that a command expects to be returned to the shell.
 type ExitStatus = int
 
 const (
 	ExitSuccess    ExitStatus = 0
 	ExitFailure    ExitStatus = 1
-	ExitUsageError            = 2
+	ExitUsageError ExitStatus = 2
 )
 
 // Command represents a single command.
 type Command interface {
-	// Name returns the command's name
+	// Name returns the command's name.
 	Name() string
 
-	// Configure configures this command.
-	Configure()
+	// Description returns a short string (less than one line) describing the command.
+	Description() string
 
-	// Run attems to run the command
+	// Usage returns a long string explaining the command and giving usage information.
+	Usage() string
+
+	// Configure configures this command.
+	Configure(*flag.FlagSet)
+
+	// Run attems to run the command.
 	Run(context.Context) ExitStatus
 }
 
 // Console represents a set of commands.
 type Console struct {
-	commands []Command
+	name        string // normally path.Base(os.Args[0])
+	description string
+	commands    map[string]Command
+
+	errWriter io.Writer // specifies where should write errors (default: os.Stderr).
 }
 
 // NewConsole returns a new Console.
-func NewConsole() *Console {
-	return &Console{}
+func NewConsole(name, description string, errWriter io.Writer) *Console {
+	return &Console{
+		name:        name,
+		description: description,
+		commands:    make(map[string]Command),
+		errWriter:   errWriter,
+	}
 }
 
 // Register registers a command.
 func (c *Console) Register(command Command) {
-	c.commands = append(c.commands, command)
+	c.commands[command.Name()] = command
 }
 
 // Run attempts to invoke registered commands.
 func (c *Console) Run(ctx context.Context, arguments []string) ExitStatus {
-	if len(arguments) == 0 {
+	if len(arguments) < 2 {
+		c.explain()
 		return ExitUsageError
 	}
 
-	if len(c.commands) == 0 {
-		return ExitSuccess
+	cmdName := arguments[1]
+
+	cmd, ok := c.commands[cmdName]
+	if !ok {
+		if cmdName == "-h" || cmdName == "-help" {
+			c.explain()
+			return ExitSuccess
+		}
+
+		fmt.Fprintf(c.errWriter, "%q is not a command, See %q.\n", cmdName, c.name+" -help")
+		return ExitUsageError
 	}
 
-	commandName := arguments[0]
+	flagSet := flag.NewFlagSet(cmdName, flag.ContinueOnError)
+	flagSet.SetOutput(c.errWriter)
+	flagSet.Usage = func() { explain(c.errWriter, cmd) }
+	cmd.Configure(flagSet)
+	if flagSet.Parse(arguments[2:]) != nil {
+		return ExitUsageError
+	}
 
-	var status int
+	return cmd.Run(ctx)
+}
+
+// Explain writes a brief description of console commands.
+func (c *Console) explain() {
+	var b strings.Builder
+	fmt.Fprintf(&b, "%s\n\nUsage:\n", c.description)
+	fmt.Fprintf(&b, "\n  %s %s\n", c.name, "[flags] <command> [command arguments]")
+
+	fmt.Fprint(&b, "\nThe commands are:\n\n")
 	for _, cmd := range c.commands {
-		if cmd.Name() != commandName {
-			continue
-		}
-
-		cmd.Configure()
-
-		if status = cmd.Run(ctx); status != ExitSuccess {
-			break
-		}
+		fmt.Fprintf(&b, "  %-10s  %s\n", cmd.Name(), cmd.Description())
 	}
+	fmt.Fprintf(&b, "\nUse \"%s <command> -h\" for more information about a command.\n", c.name)
 
-	return status
+	fmt.Fprint(c.errWriter, b.String())
+}
+
+// explain prints a brief description of a single command.
+func explain(w io.Writer, cmd Command) {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "%s\n\nUsage:\n", cmd.Description())
+	fmt.Fprintf(&b, "\n  %s\n\n", cmd.Usage())
+
+	f := flag.NewFlagSet(cmd.Name(), flag.PanicOnError)
+	f.SetOutput(&b)
+	cmd.Configure(f)
+	f.PrintDefaults()
+
+	fmt.Fprint(w, b.String())
 }
--- infrastructure/console/console_test.go
+++ infrastructure/console/console_test.go
@@ -1,148 +1,384 @@
 package console
 
 import (
+	"bytes"
 	"context"
+	"flag"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestConsole(t *testing.T) {
-	t.Run("run with no registered command", func(t *testing.T) {
-		console := NewConsole()
-
-		ctx := context.Background()
-		exitCode := console.Run(ctx, []string{""})
-
-		if exitCode != 0 {
-			t.Error("unexpected exit code")
-		}
-	})
-
-	t.Run("run with no arguments", func(t *testing.T) {
-		console := NewConsole()
-		ctx := context.Background()
-
-		if exitCode := console.Run(ctx, []string{}); exitCode != 2 {
-			t.Error("unexpected exit code")
-		}
-	})
-
-	t.Run("register commands", func(t *testing.T) {
-		console := NewConsole()
-		ctx := context.Background()
-
-		commands := []Command{
-			&MockCommand{
-				name: "test-1",
-			},
-			&MockCommand{},
-			&MockCommand{
-				name: "test-1",
-			},
-			&MockCommand{
-				name: "test-2",
-			},
-			&MockCommand{},
-		}
-
-		for i := range commands {
-			console.Register(commands[i])
-		}
-
-		const cmdName string = "test-1"
-		arguments := []string{"test-1"}
-		if exitCode := console.Run(ctx, arguments); exitCode != 0 {
-			t.Errorf("unexpected exit code %d", exitCode)
-		}
-
-		for i := range commands {
-			cmd := commands[i].(*MockCommand)
-
-			if cmd.Name() == cmdName && cmd.runCount != 1 {
-				t.Errorf("command at index %d should be invoked exactly 1 but invoked %d", i, cmd.runCount)
+	t.Run("help", func(t *testing.T) {
+		t.Run("console help without registered command", func(t *testing.T) {
+			testCases := []struct {
+				name               string
+				arguments          []string
+				registeredCommands []Command
+				exitStatus         int
+				outputErr          string
+			}{
+				{
+					name:      "-h flag",
+					arguments: []string{"", "-h"},
+				},
+				{
+					name:      "-help",
+					arguments: []string{"", "-h"},
+				},
 			}
 
-			if cmd.Name() != cmdName && cmd.runCount > 0 {
-				t.Errorf("command at index %d should not be invoked but invoked %d", i, cmd.runCount)
+			for _, testCase := range testCases {
+				t.Run(testCase.name, func(t *testing.T) {
+					var errWriter bytes.Buffer
+					console := NewConsole("Test", "Test description", &errWriter)
+
+					if exitStatus := console.Run(context.Background(), testCase.arguments); exitStatus != 0 {
+						t.Errorf("unexpected exit code, want %d got %d", 0, exitStatus)
+					}
+
+					want := testdata(t, "help-without-commands.txt")
+					got := errWriter.String()
+					if diff := cmp.Diff(want, got); diff != "" {
+						t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+					}
+				})
 			}
-		}
+		})
+
+		t.Run("console help with registered command", func(t *testing.T) {
+			testCases := []struct {
+				name               string
+				arguments          []string
+				registeredCommands []Command
+				exitStatus         int
+				outputErr          string
+			}{
+				{
+					name:      "-h flag",
+					arguments: []string{"", "-h"},
+				},
+				{
+					name:      "-help",
+					arguments: []string{"", "-h"},
+				},
+			}
+
+			for _, testCase := range testCases {
+				t.Run(testCase.name, func(t *testing.T) {
+					var errWriter bytes.Buffer
+					console := NewConsole("Test", "Test description", &errWriter)
+
+					var (
+						boolArg bool
+						intArg  int
+					)
+
+					command := NewSpyCommand(
+						"test-command",
+						"this is a test description",
+						"this is a test usage",
+						0,
+						func(fs *flag.FlagSet) {
+							fs.BoolVar(&boolArg, "boolArg", false, "test bool argument")
+							fs.IntVar(&intArg, "intArg", 666, "test int argument")
+						},
+					)
+
+					console.Register(command)
+					if exitStatus := console.Run(context.Background(), testCase.arguments); exitStatus != 0 {
+						t.Errorf("unexpected exit code, want %d got %d", 0, exitStatus)
+					}
+
+					want := testdata(t, "help-with-commands.txt")
+					got := errWriter.String()
+					if diff := cmp.Diff(want, got); diff != "" {
+						t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+					}
+				})
+			}
+		})
 	})
 
-	t.Run("will stop if a command returns non zero code", func(t *testing.T) {
-		console := NewConsole()
-		ctx := context.Background()
-
-		commands := []Command{
-			&MockCommand{},
-			&MockCommand{
-				exitCode: 1,
+	t.Run("invalid attempts", func(t *testing.T) {
+		testCases := []struct {
+			name               string
+			arguments          []string
+			registeredCommands []Command
+			exitStatus         int
+			outputErr          string
+		}{
+			{
+				name:       "no arguments",
+				arguments:  []string{},
+				exitStatus: 2,
+				outputErr:  testdata(t, "help-without-commands.txt"),
 			},
-			&MockCommand{},
+			{
+				name:       "0-either only binary or command",
+				arguments:  []string{""},
+				exitStatus: 2,
+				outputErr:  testdata(t, "help-without-commands.txt"),
+			},
+			{
+				name:       "1-either only binary or command",
+				arguments:  []string{"command"},
+				exitStatus: 2,
+				outputErr:  testdata(t, "help-without-commands.txt"),
+			},
+			{
+				name:       "not registered command (help)",
+				arguments:  []string{"", "help"},
+				exitStatus: 2,
+				outputErr:  "\"help\" is not a command, See \"Test -help\".\n",
+			},
+			{
+				name:       "not registered command with -h flag",
+				arguments:  []string{"", "command", "-h"},
+				exitStatus: 2,
+				outputErr:  "\"command\" is not a command, See \"Test -help\".\n",
+			},
+			{
+				name:       "not registered command with -help flag",
+				arguments:  []string{"binary", "command", "-help"},
+				exitStatus: 2,
+				outputErr:  "\"command\" is not a command, See \"Test -help\".\n",
+			},
 		}
 
-		for i := range commands {
-			console.Register(commands[i])
-		}
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				var errWriter bytes.Buffer
+				console := NewConsole("Test", "Test description", &errWriter)
 
-		if exitCode := console.Run(ctx, []string{""}); exitCode != 1 {
-			t.Error("unexpected exit code")
-		}
+				if exitStatus := console.Run(context.Background(), testCase.arguments); exitStatus != testCase.exitStatus {
+					t.Errorf("unexpected exit code, want %d got %d", testCase.exitStatus, exitStatus)
+				}
 
-		if commands[0].(*MockCommand).runCount != 1 {
-			t.Error("command at index 0 not invoked")
-		}
-
-		if commands[1].(*MockCommand).runCount != 1 {
-			t.Error("command at index 1 not invoked")
-		}
-
-		if commands[2].(*MockCommand).runCount != 0 {
-			t.Errorf("command at index 2 should not be invoked")
+				want := testCase.outputErr
+				got := errWriter.String()
+				if diff := cmp.Diff(want, got); diff != "" {
+					t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+				}
+			})
 		}
 	})
 
-	t.Run("configure", func(t *testing.T) {
-		console := NewConsole()
-		ctx := context.Background()
+	t.Run("exit status will be returned to caller", func(t *testing.T) {
+		statuses := []int{0, 1, 2, 3, 4}
 
-		commands := []Command{
-			&MockCommand{},
-			&MockCommand{},
-		}
+		for _, status := range statuses {
+			var errWriter bytes.Buffer
+			console := NewConsole("Test", "Test description", &errWriter)
 
-		for i := range commands {
-			console.Register(commands[i])
-		}
+			command := NewSpyCommand(
+				"command",
+				"this is a test description",
+				"this is a test usage",
+				status,
+				nil,
+			)
 
-		if exitCode := console.Run(ctx, []string{""}); exitCode != 0 {
-			t.Errorf("unexpected exit code %d", exitCode)
-		}
+			console.Register(command)
 
-		for i := range commands {
-			cmd := commands[i].(*MockCommand)
-			if cmd.configureCount != 1 {
-				t.Errorf("command at index %d configure method should be called 1 but is called %d", i, cmd.configureCount)
+			if exitStatus := console.Run(context.Background(), []string{"binary-name", "command"}); exitStatus != status {
+				t.Errorf("unexpected exit code, want %d got %d", status, exitStatus)
+			}
+
+			want := ""
+			got := errWriter.String()
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+			}
+
+			if command.NameCount != 1 {
+				t.Errorf("%q method should be called once", "Name")
+			}
+
+			if command.DescriptionCount != 0 {
+				t.Errorf("%q method should not be called", "Description")
+			}
+
+			if command.UsageCount != 0 {
+				t.Errorf("%q method should not be called", "Usage")
+			}
+
+			if command.RunCount != 1 {
+				t.Errorf("%q method should be called once", "Run")
+			}
+
+			if command.ConfigureCount != 1 {
+				t.Errorf("%q method should be called once", "Configure")
 			}
 		}
+	})
+
+	t.Run("test command arguments", func(t *testing.T) {
+		var errWriter bytes.Buffer
+		console := NewConsole("Test", "Test description", &errWriter)
+
+		var (
+			boolArg bool
+			intArg  int
+		)
+
+		command := NewSpyCommand(
+			"test-command",
+			"this is a test description",
+			"this is a test usage",
+			0,
+			func(fs *flag.FlagSet) {
+				fs.BoolVar(&boolArg, "boolArg", false, "test bool argument")
+				fs.IntVar(&intArg, "intArg", 666, "test int argument")
+			},
+		)
+
+		console.Register(command)
+
+		t.Run("args should be filled with provided values", func(t *testing.T) {
+			errWriter.Reset()
+			arguments := []string{"binary-name", "test-command", "-intArg", "100", "-boolArg", "true"}
+			if exitStatus := console.Run(context.Background(), arguments); exitStatus != 0 {
+				t.Errorf("unexpected exit code, want %d got %d", 0, exitStatus)
+			}
+
+			if command.RunCount != 1 {
+				t.Errorf("%q method should be called once", "Run")
+			}
+
+			if command.ConfigureCount != 1 {
+				t.Errorf("%q method should be called once", "Configure")
+			}
+
+			want := ""
+			got := errWriter.String()
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+			}
+
+			if boolArg != true {
+				t.Errorf("unexpected argument, want true got false")
+			}
+
+			if intArg != 100 {
+				t.Errorf("unexpected argument, want %d got %d", 100, intArg)
+			}
+		})
+
+		t.Run("flag type mismatch", func(t *testing.T) {
+			errWriter.Reset()
+			arguments := []string{"binary-name", "test-command", "-intArg", "100.2", "-boolArg", "true"}
+			if exitStatus := console.Run(context.Background(), arguments); exitStatus != 2 {
+				t.Errorf("unexpected exit code, want %d got %d", 0, exitStatus)
+			}
+
+			want := testdata(t, "flag-type-mismatch.txt")
+			got := errWriter.String()
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+			}
+
+			if boolArg != false {
+				t.Errorf("unexpected argument, want false got true")
+			}
+
+			if intArg != 666 {
+				t.Errorf("unexpected argument, want %d got %d", 666, intArg)
+			}
+		})
+
+		t.Run("non existing arg", func(t *testing.T) {
+			errWriter.Reset()
+			arguments := []string{"binary-name", "test-command", "-nonexisting", "abc", "-intArg", "100"}
+			if exitStatus := console.Run(context.Background(), arguments); exitStatus != ExitUsageError {
+				t.Errorf("unexpected exit code, want %d got %d", ExitUsageError, exitStatus)
+			}
+
+			want := testdata(t, "flag-provided-but-not-defined.txt")
+			got := errWriter.String()
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("console error output mismatch (-want +got):\n%s", diff)
+			}
+
+			if boolArg != false {
+				t.Errorf("unexpected argument, want false got true")
+			}
+
+			if intArg != 666 {
+				t.Errorf("unexpected argument, want %d got %d", 666, intArg)
+			}
+		})
 	})
 }
 
-type MockCommand struct {
-	name           string
-	exitCode       int
-	runCount       int
-	configureCount int
+type SpyCommand struct {
+	name          string
+	description   string
+	usage         string
+	exitStatus    int
+	configureFunc func(*flag.FlagSet)
+
+	NameCount        int
+	DescriptionCount int
+	UsageCount       int
+	RunCount         int
+	ConfigureCount   int
 }
 
-func (c *MockCommand) Name() string {
+func NewSpyCommand(
+	name, description,
+	usage string,
+	exitStatus int,
+	configureFunc func(*flag.FlagSet),
+) *SpyCommand {
+	return &SpyCommand{
+		name:          name,
+		description:   description,
+		usage:         usage,
+		exitStatus:    exitStatus,
+		configureFunc: configureFunc,
+	}
+}
+
+func (c *SpyCommand) Name() string {
+	c.NameCount++
 	return c.name
 }
 
-func (c *MockCommand) Configure() {
-	c.configureCount++
+func (c *SpyCommand) Description() string {
+	c.DescriptionCount++
+	return c.description
 }
 
-func (c *MockCommand) Run(ctx context.Context) int {
-	c.runCount++
+func (c *SpyCommand) Usage() string {
+	c.UsageCount++
+	return c.usage
+}
 
-	return c.exitCode
+func (c *SpyCommand) Configure(flagSet *flag.FlagSet) {
+	c.ConfigureCount++
+
+	if c.configureFunc != nil {
+		c.configureFunc(flagSet)
+	}
+}
+
+func (c *SpyCommand) Run(ctx context.Context) ExitStatus {
+	c.RunCount++
+
+	return c.exitStatus
+}
+
+func testdata(t *testing.T, filename string) string {
+	t.Helper()
+
+	b, err := os.ReadFile(fmt.Sprintf("testdata/%s", filename))
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	return string(b)
 }
