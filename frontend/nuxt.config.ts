154c61454dfa0f64edbcf13ac9ffc3de4a7919ef
Mahdi Khanzadi <khanzadimahdi@gmail.com>
2024-04-29 11:38:33 +0200

    extract css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  build: {
+    
+  },
+  vite: {
+    build: {
+      cssCodeSplit: false,
+   },
+  },
+  webpack: {
+    extractCSS: true,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
+    }
+  },
   modules: ['@vueuse/nuxt', "nuxt-tiptap-editor"],
   runtimeConfig: {
     internalApiBaseUrl: '',
