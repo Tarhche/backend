cbdd628ef89525d60ef0412150c2e9628a4be60e
esakh <esakhanzadi@gmail.com>
2024-07-29 16:19:28 +0330

    covert static comments to dynamic comments

--- frontend/components/comments/index.vue
+++ frontend/components/comments/index.vue
@@ -4,12 +4,10 @@
             <span class="fa-regular fa-comment"></span>
             <span class="mx-1">دیدگاه ها</span>
         </h5>
-
         <form v-if="params.isLogin" class="my-3">
             <textarea class="form-control mb-3" placeholder="دیدگاه خود را اینجا بنویسید" rows="3" required></textarea>
             <button class="btn btn-success" type="submit">ثبت دیدگاه</button>
         </form>
-
         <div v-else class="alert alert-light">
             <i class="fa-regular fa-bell fa-shake fa-xl"></i>
             <span class="mx-1">برای ثبت دیدگاه خود</span>
@@ -22,54 +20,33 @@
 </template>
 
 <script lang="ts" setup>
-const comments = [
-  {
-    img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-    name: "افشار زند",
-    time: "'2024/05/20'",
-    text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است.",
-    sub:[
-      {
-        img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-        name: "افشار زند",
-        time: "'2024/05/20'",
-        text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است.",
-        sub:[
-          {
-            img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-            name: "عیسی خانزادی",
-            time: "'2024/05/20'",
-            text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است."
-          },
-        ]
-      },
-      {
-        img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-        name: "افشار زند",
-        time: "'2024/05/20'",
-        text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است."
-      },
-    ]
-  },
-  {
-    img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-    name: "عیسی خانزادی",
-    time: "'2024/05/20'",
-    text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است."
-  },
-  {
-    img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-    name: "مهدی خانزادی",
-    time: "'2024/05/20'",
-    text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است."
-  },
-  {
-    img: 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp',
-    name: "صادق خانزادی",
-    time: "'2024/05/20'",
-    text: "طراحان معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه خواهد بود و فونت ها و اندازه ها چگونه در نظر گرفته شده است."
-  },
-]
+const {data} = defineProps(['data'])
+const comments = (data && data.length) ?  createTree(data) : ""
+
+function createTree(comments:[]) {
+  const map = new Map();
+  comments.forEach(comment => map.set(comment.uuid, comment));
+
+  const tree:[] = [];
+  comments.forEach(comment => {
+    const parent = map.get(comment.parent_uuid);
+    if (!parent) {
+      tree.push(comment);
+    }
+  });
+
+  // تکمیل کردن زیرمجموعه‌ها
+  tree.forEach((node:object) => {
+    const fillSubtree = (node) => {
+      node.sub = comments.filter(comment => comment.parent_uuid === node.uuid);
+      node.sub.forEach(fillSubtree);
+    };
+    fillSubtree(node);
+  });
+
+  return tree;
+}
+
 const params = reactive({
         isLogin: useAuth().isLogin(),
 })
