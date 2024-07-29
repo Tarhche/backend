6bcd382c021a410c23e7bbd772bf0e7f41bc1f0d
esakh <esakhanzadi@gmail.com>
2024-07-29 19:19:20 +0330

    wip create comments in frontEnd

--- frontend/components/comments/index.vue
+++ frontend/components/comments/index.vue
@@ -4,9 +4,10 @@
             <span class="fa-regular fa-comment"></span>
             <span class="mx-1">دیدگاه ها</span>
         </h5>
-        <form v-if="params.isLogin" class="my-3">
-            <textarea class="form-control mb-3" placeholder="دیدگاه خود را اینجا بنویسید" rows="3" required></textarea>
-            <button class="btn btn-success" type="submit">ثبت دیدگاه</button>
+        <form v-if="params.isLogin" class="my-3 d-flex flex-column" @submit.prevent="sendComment">
+            <textarea class="form-control mb-1"  placeholder="دیدگاه خود را اینجا بنویسید" rows="3" required v-model="commentData.body"></textarea>
+            <span class="error text-danger" v-if="commentData.error" >متن پیام حداقل باید بیشتر باشد .</span>
+            <button class="btn btn-success align-self-start mt-3" type="submit">ثبت دیدگاه</button>
         </form>
         <div v-else class="alert alert-light">
             <i class="fa-regular fa-bell fa-shake fa-xl"></i>
@@ -15,15 +16,19 @@
             <span>یا</span>
             <NuxtLink class="mx-1" to="/auth/login">وارد شوید</NuxtLink>
         </div>
-    <comments-item v-for="(item , index) in comments" :key="index" :data="item"  v-if="comments && comments.length && params.isLogin"/>
+    <comments-item v-for="(item , index) in comments" :key="index" :data="item"  v-if="comments && comments.length"/>
   </section>
 </template>
 
 <script lang="ts" setup>
 const {data} = defineProps(['data'])
+const commentData = reactive({
+  body:"",
+  error:false
+})
 const comments = (data && data.length) ?  createTree(data) : ""
-
 function createTree(comments:[]) {
+
   const map = new Map();
   comments.forEach(comment => map.set(comment.uuid, comment));
 
@@ -46,7 +51,14 @@ function createTree(comments:[]) {
 
   return tree;
 }
-
+const sendComment = ()=>{
+  if (commentData.body.length > 5){
+    $fetch(useApiUrlResolver().resolve('comments') , {
+      method:'POST',
+      body:JSON.stringify({body:commentData.body})
+    })
+  }
+}
 const params = reactive({
         isLogin: useAuth().isLogin(),
 })
