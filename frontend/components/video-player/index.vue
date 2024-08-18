508cf40953c03b098b2321b52d2b7f3501893c1c
Mahdi Khanzadi <khanzadimahdi@gmail.com>
2024-08-18 12:30:26 +0200

    fix node dependencies

---
+++ frontend/components/video-player/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <video id="video-player" class="video-js" controls preload="auto" :poster="poster" data-setup="{}">
+    <source :src="video" type="video/mp4"/>
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+    </p>
+  </video>
+</template>
+
+<script setup lang="ts">
+// import videojs from "video.js";
+
+const {video, poster} = defineProps({
+  video: String,
+  poster: String,
+})
+
+
+// onMounted(() => {
+//   const player = videojs("video-player", {}, () => {
+//     player.log('onPlayerReady');
+//   })
+// })
+</script>
--- frontend/pages/video-test.vue
+++ frontend/pages/video-test.vue
@@ -4,13 +4,9 @@ const videoUrl ='http://commondatastorage.googleapis.com/gtv-videos-bucket/sampl
 <template>
   <div class="container">
     <div class="row">
-        <div class="col-md-8 mx-auto " :style="{height: '300px'}">
-          <videojs :video="videoUrl"  />
+        <div class="col-md-8 mx-auto">
+          <video-player width="100%" :video="videoUrl"  />
         </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
