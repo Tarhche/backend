1d67daa26f43d83191f442f2e3dac5ef22cc1d72
Eisa <esakhanzadi@gmail.com>
2024-08-19 20:37:04 +0330

    create component vue-plyr

---
+++ frontend/components/video-player/index.client.vue
@@ -0,0 +1,40 @@
+<template>
+  <vue-plyr :options="options" >
+    <video controls
+           playsinline
+           :data-poster="poster">
+      <source
+          size="1080p"
+          :src="video"
+          type="video/mp4">
+      <!--<source size="576"
+              src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+              type="video/mp4">
+      <source size="720"
+              src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
+              type="video/mp4">
+      <track default
+             kind="captions"
+             label="English captions"
+             src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
+             srclang="en">
+      <track kind="captions"
+             label="Légendes en français"
+             src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"
+             srclang="fr"> -->
+      <a download=""
+         :href="video">
+        Download
+      </a>
+    </video>
+  </vue-plyr>
+</template>
+
+<script setup lang="ts">
+import VuePlyr from '@skjnldsv/vue-plyr'
+const {video, poster} = defineProps({
+  video: String,
+  poster: String,
+})
+const options = {quality: {default: '1080p'}}
+</script>
