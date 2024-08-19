f3c333c752a3a9bb42716f63b816b58e67c7ccea
Eisa <esakhanzadi@gmail.com>
2024-08-19 20:37:54 +0330

    create page for showing vue-plyr

--- frontend/pages/video-test.vue
+++ frontend/pages/video-test.vue
@@ -1,12 +1,9 @@
-<script setup>
-const videoUrl ='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+<script setup lang="ts">
+const url = 'https://caspian14.cdn.asset.aparat.com/aparat-video/d60078eeb60aa84d16bbfd03e6f9114360412550-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImM3ZTVkZGFjN2FhM2U4YjMwMjNkNzAzMDJlZTEzM2Q0IiwiZXhwIjoxNzI0MTExODY5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.IWLkIlBqV6lm1sRvQfYWHPe9DXDyJurveXOgIoQdX0M'
 </script>
+
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col-md-8 mx-auto">
-          <video-player width="100%" :video="videoUrl"  />
-        </div>
-    </div>
+  <div class="container" style="height: 500px;">
+    <video-player :video="url"/>
   </div>
 </template>
