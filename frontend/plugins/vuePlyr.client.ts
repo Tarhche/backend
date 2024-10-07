629eeb5606b4904769ce261e5b3cddbcaaadbc1d
sina-saeedi <sina.sa.dev@gmail.com>
2024-10-07 14:56:35 +0330

    refactor: cleanup Nuxt.js files in preparation for Next.js migration

--- frontend/plugins/vuePlyr.client.ts
+++
@@ -1,6 +0,0 @@
-import VuePlyr from '@skjnldsv/vue-plyr'
-import '@skjnldsv/vue-plyr/dist/vue-plyr.css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('vue-plyr' ,VuePlyr)
-})
