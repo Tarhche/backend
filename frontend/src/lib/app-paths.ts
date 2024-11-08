7d9692e97776e8f4dafde967df27686847f3dde3
sina-saeedi <sina.sa.dev@gmail.com>
2024-11-08 18:35:29 +0330

    feat: add app-path object to store all application paths in a single place

---
+++ frontend/src/lib/app-paths.ts
@@ -0,0 +1,26 @@
+type Path = string | ((...args: any[]) => string);
+
+export type PathSchema = Record<string, Path | Record<string, Path>>;
+
+export const APP_PATHS = {
+  home: "/",
+  articles: {
+    index: "articles",
+    detail: (uuid: string) => `articles/${uuid}`,
+  },
+  auth: {
+    login: "auth/login",
+    register: "auth/register",
+    verify: "auth/verify",
+    resetPassword: "auth/reset-password",
+    frogotPassword: "auth/forgot-password",
+  },
+  hashtags: {
+    index: "hashtags",
+  },
+  dashboard: {
+    index: "dashboard",
+    articles: "dashboard/articles",
+    articlesDetail: (uuid: string) => `dashboard/articles/${uuid}`,
+  },
+} as const satisfies PathSchema;
