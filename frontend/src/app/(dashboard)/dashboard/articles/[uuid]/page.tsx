34e72f85b58f3a423ab6b91646c865264291bd09
sina-saeedi <sina.sa.dev@gmail.com>
2024-11-08 18:36:03 +0330

    feat: add the initial articles and article detail pages

---
+++ frontend/src/app/(dashboard)/dashboard/articles/[uuid]/page.tsx
@@ -0,0 +1,7 @@
+import {Title} from "@mantine/core";
+
+function ArticleDetalPage() {
+  return <Title>جزییات مقاله</Title>;
+}
+
+export default ArticleDetalPage;
---
+++ frontend/src/app/(dashboard)/dashboard/articles/page.tsx
@@ -0,0 +1,5 @@
+import {Title} from "@mantine/core";
+
+export default function ArticlesPage() {
+  return <Title>مقالات</Title>;
+}
