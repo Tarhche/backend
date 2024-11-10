5fd9bfb91cbd1ce8c05005bd1f93f215e91c48b8
sina-saeedi <sina.sa.dev@gmail.com>
2024-11-10 09:45:24 +0330

    refactor: remove the NEXT_PUBLIC prefix

--- frontend/src/constants/envs.ts
+++ frontend/src/constants/envs.ts
@@ -1,4 +1,4 @@
 export const INTERNAL_BACKEND_URL = process.env.INTERNAL_BACKEND_BASE_URL;
 export const EXTERNAL_BACKEND_URL =
   process.env.NEXT_PUBLIC_EXTERNAL_BACKEND_BASE_URL;
-export const FILES_PUBLIC_URL = `${process.env.NEXT_PUBLIC_FILES_BASE_URL}/files`;
+export const FILES_PUBLIC_URL = `${process.env.FILES_BASE_URL}/files`;
