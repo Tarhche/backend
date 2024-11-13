a41ea444b77405cd36a8af742f74bfe19f300c9d
sina-saeedi <sina.sa.dev@gmail.com>
2024-11-13 13:32:31 +0330

    feat: add UserAvatar component

--- frontend/src/components/auth-user-avatar.tsx
+++ frontend/src/components/auth-user-avatar.tsx
@@ -1,11 +1,7 @@
 "use client";
-import Image from "next/image";
-import {useState} from "react";
-import {Avatar, Skeleton, useMantineTheme} from "@mantine/core";
-import BoringAvatar from "boring-avatars";
+import {Skeleton} from "@mantine/core";
+import {UserAvatar} from "./user-avatar";
 import {useInit} from "@/hooks/data/init";
-import {FILES_PUBLIC_URL} from "@/constants/envs";
-import classes from "./auth-user-avatar.module.css";
 
 type Props = {
   width?: number;
@@ -14,36 +10,14 @@ type Props = {
 
 export function AuthUserAvatar({width = 45, height = 45}: Props) {
   const {data, isLoading} = useInit();
-  const theme = useMantineTheme();
-  const colors = Object.values(theme.colors).map((c) => c[6]);
-  const [hasImageFailed, setHasImageFailed] = useState(false);
   if (isLoading) {
     return <Skeleton circle width={width} height={height} />;
   }
 
   if (data?.status === "authenticated") {
-    const {avatar, name, email} = data.profile;
-    if (hasImageFailed || avatar === undefined) {
-      return (
-        <Avatar src={null} w={width} h={height}>
-          <BoringAvatar
-            variant="beam"
-            name={email}
-            size={width}
-            colors={colors}
-          />
-        </Avatar>
-      );
-    }
+    const {avatar, email} = data.profile;
     return (
-      <Image
-        src={`${FILES_PUBLIC_URL}/${avatar}`}
-        alt={name}
-        width={width}
-        height={height}
-        className={classes.avatar}
-        onError={() => setHasImageFailed(true)}
-      />
+      <UserAvatar email={email} src={avatar} width={width} height={height} />
     );
   }
 
